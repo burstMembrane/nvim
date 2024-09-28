e87060dd4d4065a580eb78e43607f5bc19d4bd62
Liam Power <liamfpower@gmail.com>
2024-09-28 12:04:52 +1000

    added lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -7,25 +7,29 @@ local M = {
 
 
 function M.config()
-  local servers = {
-    "lua_ls",
-    "cssls",
-    "html",
-    "ts_ls",
-    "pyright",
-    "bashls",
-    "jsonls",
-  }
+  local lsp = require("lsp-zero")
+	lsp.preset("recommended")
 
+	require("lsp-zero").setup()
+
+	require("mason-lspconfig").setup_handlers {
+		function (server_name)
+		    require "lspconfig" [server_name].setup {}
+		end
+	}
+
+
+	require("mason-lspconfig").setup {
+      ensure_installed = M.servers,
+      automatic_installation = true,
+	}
   require("mason").setup {
     ui = {
       border = "rounded",
     },
   }
 
-  require("mason-lspconfig").setup {
-    ensure_installed = servers,
-  }
+  require("mason-lspconfig").setup()
 end
 
 return M

c57b44b360a19f9f2885aafb6c701ca6b33e7944
Liam Power <liamfpower@gmail.com>
2024-09-28 14:07:17 +1000

    added telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+local M = {
+'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  lazy = false, 
+   dependencies = { 'nvim-lua/plenary.nvim' }
+}
+
+function M.config()
+ local wk = require "which-key"
+  wk.add ({
+    {"<leader>f", group = "Find"},
+    {"<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files", mode = "n"},
+    {"<leader>fd", "<cmd>Telescope fd<CR>", desc = "Find Directories"},
+    {"<leader>t", "<cmd>Telescope live_grep<CR>", desc = "Find Text"}
+   })
+ 
+ end
+
+return M

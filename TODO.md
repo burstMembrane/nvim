cc54c8b9cdc09059ab0921596f8699a8bf8068aa
Liam Power <liamfpower@gmail.com>
2024-09-29 17:05:24 +1000

    Added bracket highlighting to TODO.md Update settings Set telescope to close on ESC instead of changing mode.

--- TODO.md
+++ TODO.md
@@ -12,6 +12,7 @@ This repo will serve as a rekindling of my bare love of coding, and I will be wo
 [x] Syntax Highlighting `treesitter`
 [x] Completion
 [x] Quickly find files
+[ ] Bracket highlighting
 [ ] Quickly create and navigate new files
 [ ] Figure out how to move lines up without cut and paste
 [ ] Figure out how to move back a word in a line
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,17 +4,36 @@ local M = {
   lazy = false,
   dependencies = { "nvim-lua/plenary.nvim" },
 }
+
 function M.config()
   local wk = require "which-key"
+
+  -- Add key mappings for Telescope under leader key
   wk.add {
-    { "<leader>f",  group = "Find" },
-    { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files",       mode = "n" },
-    { "<leader>fd", "<cmd>Telescope fd<CR>",         desc = "Find Directories" },
+    { "<leader>f", group = "Find" },
+    { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files", mode = "n" },
+    { "<leader>fe", "<cmd>Telescope current_buffer_fuzzy_find<CR>", desc = "Find Text (Current Buffer)" },
     { "<leader>fs", "<cmd>Telescope treesitter<CR>", desc = "Find Symbols" },
-    { "<leader>fc", "<cmd>Telescope live_grep<CR>",  desc = "Find Text" },
-    { "<leader>fb", "<cmd>Telescope buffers<CR>",    desc = "Find Buffers" },
-    { "<leader>fr", "<cmd>Telescope oldfiles<CR>",   desc = "Find Recent Files" },
-    { "<leader>fh", "<cmd>Telescope help_tags<CR>",  desc = "Find Help" },
+    { "<leader>fd", "<cmd>Telescope live_grep<CR>", desc = "Find Text (CWD)" },
+    { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Find Buffers" },
+    { "<leader>fr", "<cmd>Telescope oldfiles<CR>", desc = "Find Recent Files" },
+    { "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "Find Help" },
+  }
+
+  -- Add Alt+p keybinding for Telescope find_files in the same format
+  wk.add {
+    { "<M-p>", "<cmd>Telescope find_files<CR>", desc = "Find Files", mode = "n" },
+  }
+
+  local actions = require "telescope.actions"
+  require("telescope").setup {
+    defaults = {
+      mappings = {
+        i = {
+          ["<esc>"] = actions.close,
+        },
+      },
+    },
   }
 end
 

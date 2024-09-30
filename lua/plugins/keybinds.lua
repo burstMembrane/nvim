29dac9155536ed305f1a7873fa93b494ad8f63da
Liam Power <liamfpower@gmail.com>
2024-09-30 22:19:54 +1000

    update config

--- lua/plugins/keybinds.lua
+++ lua/plugins/keybinds.lua
@@ -19,7 +19,7 @@ function M.config()
   wk.add {
     { "<leader>q", "<cmd>q<cr>", desc = "Quit" }, -- Quit
     { "<leader>w", "<cmd>w<cr>", desc = "Write" }, -- Write
-    { "<leader>wq", "<cmd>wq<cr>", desc = "Write Quit" }, -- Write/Quit
+    -- { "<leader>wq", "<cmd>wq<cr>", desc = "Write Quit" }, -- Write/Quit
     { "<leader>sa", ":normal ggVG<cr>", desc = "Select All" },
     { "<leader>sl", ":normal 0v$<cr>", desc = "Select Line" },
     { "<leader>d", "<cmd>lua show_diagnostics()<cr>", desc = "show error or warning under cursor" }, -- Show diagnostics

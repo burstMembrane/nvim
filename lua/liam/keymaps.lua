772599546f46d47f62ab9a18db827f73e6579bd8
Liam Power <liamfpower@gmail.com>
2024-10-03 22:55:53 +1000

    hard mode- remove arrow keys

--- lua/liam/keymaps.lua
+++ lua/liam/keymaps.lua
@@ -25,3 +25,9 @@ vim.api.nvim_set_keymap("n", "<C-p>", "<cmd>Telescope find_files<CR>", { noremap
 
 -- add C-S-f for find in file (current buffer)
 vim.api.nvim_set_keymap("n", "<C-f>", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { noremap = true, silent = true })
+
+-- hard mode - remove all arrow keymaps
+vim.api.nvim_set_keymap("n", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Right>", "<Nop>", { noremap = true, silent = true })

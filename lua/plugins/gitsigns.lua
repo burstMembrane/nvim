3a3eb5d9455799e17d84a966d28a4d94e352e24f
Liam <liamfpower@gmail.com>
2024-10-06 19:03:06 +1100

    add gitsigns, find and replace and colorscheme

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,54 @@
+local M = {
+  "lewis6991/gitsigns.nvim",
+  event = "BufEnter",
+  cmd = "Gitsigns",
+}
+M.config = function()
+  local icons = require "user.icons"
+  local wk = require "which-key"
+  local gitsigns = require "gitsigns"
+  local function map(mode, l, r, opts)
+    opts = opts or {}
+    opts.buffer = bufnr
+    vim.keymap.set(mode, l, r, opts)
+  end
+  map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+  map('n', '<leader>hd', gitsigns.diffthis)
+
+  require("gitsigns").setup {
+    signs = {
+      add = {
+        text = icons.ui.BoldLineMiddle,
+      },
+      change = {
+        text = icons.ui.BoldLineDashedMiddle,
+      },
+      delete = {
+        text = icons.ui.TriangleShortArrowRight,
+      },
+      topdelete = {
+        text = icons.ui.TriangleShortArrowRight,
+      },
+      changedelete = {
+        text = icons.ui.BoldLineMiddle,
+      },
+    },
+    watch_gitdir = {
+      interval = 1000,
+      follow_files = true,
+    },
+    attach_to_untracked = true,
+    current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+    update_debounce = 200,
+    max_file_length = 40000,
+    preview_config = {
+      border = "rounded",
+      style = "minimal",
+      relative = "cursor",
+      row = 0,
+      col = 1,
+    },
+  }
+end
+
+return M

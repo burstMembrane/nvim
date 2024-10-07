e5937a231d9fef5933329f8d11121fb0f9c9bfc5
Liam <liamfpower@gmail.com>
2024-10-08 00:15:19 +1100

    Update copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -17,7 +17,7 @@ function M.config()
       hide_during_completion = true,
       debounce = 75,
       keymap = {
-        accept = "<M-l>",
+        accept = "<C-Right>",
         accept_word = false,
         accept_line = false,
         next = "<M-]>",
---
+++ lua/plugins/lsp_signature.lua
@@ -0,0 +1,30 @@
+local M = {
+  "ray-x/lsp_signature.nvim",
+  event = "VeryLazy",
+  opts = {},
+}
+
+
+function M.config()
+  vim.keymap.set({ 'n' }, '<C-k>', function()
+    require('lsp_signature').toggle_float_win()
+  end, { silent = true, noremap = true, desc = 'toggle signature' })
+
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local bufnr = args.buf
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if vim.tbl_contains({ 'null-ls' }, client.name) then -- blacklist lsp
+        return
+      end
+      require("lsp_signature").on_attach({
+        -- ... setup options here ...
+      }, bufnr)
+    end,
+  })
+
+  require "lsp_signature".setup({
+  })
+end
+
+return M

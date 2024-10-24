60a18fdb00f4c15c23a8081f7e5f79ef439f6d60
Liam Power <liamfpower@gmail.com>
2024-10-24 22:33:09 +1100

    feat: add hardtime

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,12 @@
+local M = {
+  "m4xshen/hardtime.nvim",
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  opts = {}
+}
+
+function M.config()
+  -- require("hardtime").setup({
+  -- })
+end
+
+return M

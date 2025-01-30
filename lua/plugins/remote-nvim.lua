6cc15d325dfed5593c3307dba9b6ce1ad91e3a56
Liam Power <liamfpower@gmail.com>
2025-01-30 20:06:23 +1100

    fix: remove remote-nvim and image.nvim

---
+++ lua/plugins/remote-nvim.lua
@@ -0,0 +1,17 @@
+local M = {
+  "amitds1997/remote-nvim.nvim",
+  version = "*",                     -- Pin to GitHub releases
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- For standard functions
+    "MunifTanjim/nui.nvim",          -- To build the plugin UI
+    "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+  },
+  config = true,
+}
+
+M.config = function()
+  require("remote-nvim").setup {}
+end
+
+
+return M

0943ac719f0c3d028d18f13f0b819cbfa1639594
Liam Power <liamfpower@gmail.com>
2025-02-06 20:31:57 +1100

    fix: remove yazi plugin, was causing slowdowns

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,9 +10,7 @@ local M = {
 function M.config()
   local harpoon = require("harpoon")
   --
-  -- -- REQUIRED
   harpoon:setup()
-  -- -- REQUIRED
   --
   vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
   vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)

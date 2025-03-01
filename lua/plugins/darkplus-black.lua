928ca6c1d78674d7faba0ac0590906528835a39a
Liam Power <liamfpower@gmail.com>
2025-03-01 16:48:46 +1100

    feat: add darkplus-black

--- lua/plugins/darkplus-black.lua
+++ lua/plugins/darkplus-black.lua
@@ -1,11 +1,14 @@
-local M = { 'dasupradyumna/midnight.nvim', lazy = false, priority = 1000 }
+-- local M = { dir = '/Users/liampower/darkplus-black.nvim/', dev = true, lazy = false, priority = 2000,
+-- }
+local M = { 'burstMembrane/darkplus-black.nvim', dev = false, lazy = false, priority = 2000,
+}
 
 function M.config()
   -- check if the environment variable is set and convert to boolean
   local is_vscode = vim.loop.os_getenv("VSCODE_INJECTION") == "1"
   -- set colorscheme based on whether we are in vscode
   if is_vscode then
-    vim.cmd.colorscheme "midnight"
+    vim.cmd.colorscheme "darkplus-black"
   end
 end
 
